0037d09f923b88cfe138cca740238f6846244d76
Eduards Gzibovskis <gzibovskis1998@gmail.com>
2025-11-20 15:40:40 +0000

    Potential issue with skippable at Step Component

--- app/Filament/Resources/Customers/Tables/CustomersTable.php
+++ app/Filament/Resources/Customers/Tables/CustomersTable.php
@@ -2,9 +2,14 @@
 
 namespace App\Filament\Resources\Customers\Tables;
 
+use Filament\Actions\Action;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Wizard;
+use Filament\Schemas\Components\Wizard\Step;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
 class CustomersTable
@@ -13,13 +18,39 @@ class CustomersTable
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name')
+                    ->label('First Name')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('email')
+                    ->label('Email Address')
+                    ->searchable()
             ])
             ->filters([
                 //
             ])
             ->recordActions([
                 EditAction::make(),
+                Action::make('delete')
+                    ->label('Delete')
+                    ->schema([
+                        Wizard::make([
+                            Step::make('reason')
+                                ->schema([
+                                    TextInput::make('reason')
+                                        ->label('Reason for Deletion')
+                                        ->required()
+                                ])
+                                ->skippable(), // <-------- Errors
+                            Step::make('confirm')
+                                ->schema([
+                                    TextInput::make('confirm')
+                                        ->label('Type "DELETE" to confirm')
+                                        ->required()
+                                ])
+                        ])
+                        ->skippable() // <-------- This is ok
+                    ])
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
